8db6c6d82b46c4e510d3701356835c2b9a6122a9
Steve Copley <steve.copley@waimea.school.nz>
2026-03-13 13:28:02 +1300

    Kotlin examples

--- docs/_plugins/docsify-kotlin-runner.js
+++ docs/_plugins/docsify-kotlin-runner.js
@@ -1,12 +1,28 @@
 (function () {
     var docsifyKotlinRunner = function (hook) {
+
+        // Transform opt-in markers in raw markdown before rendering
+        hook.beforeEach(function (content) {
+            content = content.replace(
+                // Match runnable code blocks
+                /^```kotlin\s+run\r?\n([\s\S]*?)^```/gm,
+                // And wrap code with fun main() {...} if needed
+                function (match, body) {
+                    // main() is not there?
+                    if (!/fun\s+main\s*\(/.test(body)) {
+                        const indented = body.replace(/^(.+)/gm, '    $1')
+                        body = 'fun main() {\n//sampleStart\n' + indented + '//sampleEnd\n}\n'
+                    }
+                    return '<div class="kotlin-run">\n\n```kotlin\n' + body + '```\n\n</div>'
+                }
+            )
+            return content
+        })
+
+        // Launcher code runner for runnable code blocks
         hook.doneEach(function () {
-            const kotlinCode = 'pre code.lang-kotlin, pre code.language-kotlin'
-
+            const kotlinCode = '.kotlin-run pre code.lang-kotlin, .kotlin-run pre code.language-kotlin'
             var kotlinCodeBlocks = document.querySelectorAll(kotlinCode)
-
-            console.log(kotlinCodeBlocks.length)
-
             if (!kotlinCodeBlocks.length) return
 
             kotlinCodeBlocks.forEach(codeBlock => {
@@ -18,9 +34,7 @@
                 codeBlock.setAttribute('match-brackets', 'true')
             })
 
-            KotlinPlayground(
-                kotlinCode
-            )
+            KotlinPlayground(kotlinCode)
         })
     }
 
